c396b1bd80d146d0ea664152cec19362ede81342
eodn <s20063@gsm.hs.kr>
2021-12-13 19:52:39 +0900

    [Publishing]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from 'styled-components';
+import Writing_box from "./components/Writing_box";
+import Text from "./components/Text";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #212529;
+  }
+  h1 {
+    font-size: 50px;
+    color: #f1f3f5;
+
+    text-align: center ;
+  }
+  button {
+      margin: 0 auto;
+      display:block;
+      background-color: #868e96;
+			padding: 15px 30px;
+      border-color: #f8f9fa;
+      border-radius: 4px;
+			font-size: 16px;
+			cursor: pointer;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Notes</h1>
+    <GlobalStyle />
+    <Writing_box />
+    <button>메모하기</button>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Text.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Inputs = styled.input`
+  
+  }
+`;
+
+function Text() {
+  return (
+    <>
+      <Inputs />
+    </>
+  );
+}
+
+export default Text;
